8e3b1266562d3f24f5f5a8498c6ed01a7570e768
Srihithl <87987067+Srihithl@users.noreply.github.com>
2021-07-28 09:58:07 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(60);
+
+  if(keyIsDown(RIGHT_ARROW)){
+    background('red');
+  }
+
+  if(keyIsDown(LEFT_ARROW)){
+    background('blue');
+  }
+
+  if(keyIsDown(UP_ARROW)){
+    background('orange');
+  }
+
+if(keyIsDown(DOWN_ARROW)){
+  background('green');
+}
+
+}
+
+
+
+
